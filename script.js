b10e910ca9bbd80580c7b2b7cc26ff5e4bf4fa32
Kivi Corn <magiciankivi@gmail.com>
2020-11-02 10:10:28 +0200

    add files

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    document.querySelector('.page').classList.remove('page');
+}
+
+// function showPage(){
+//     document.querySelector('.page').classList.remove('page');
+// }
