c3cf5fab763493db164112f1fedd7b730c3ebe49
Kivi Corn <magiciankivi@gmail.com>
2020-11-12 01:19:14 +0200

    add login page and edit links to forms and pages and fix hamberger bug

--- script.js
+++ script.js
@@ -18,3 +18,78 @@ function check(e){
 function googleTranslateElementInit() {
     new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
   }
+
+
+// // // // // // // //
+// // LOGIN MODAL// //
+// // // // // // // //
+
+const loginBtn = document.querySelectorAll('.login-button');
+const modal = document.querySelector('.login-container');
+const usernameUI = document.querySelector('#username');
+const passwordUI = document.querySelector('#password');
+const submitBtn = document.querySelector('.submit-btn');
+submitBtn.addEventListener('click', checkInput);
+window.addEventListener('click', closeModal);
+
+// listen to ALL "client zone" buttons
+loginBtn.forEach(function(element){
+    element.addEventListener('click', openModal);
+})
+
+function openModal(){
+    modal.style.display = 'block';
+}
+
+function closeModal(e){
+    if (e.target == modal){
+        modal.style.display = 'none';
+    }
+}
+
+passwordUI.addEventListener('input', function(){
+    const usernameInput = usernameUI.value.toLowerCase();
+    const correctUsername = 'talmidim';
+    const loginBox = document.querySelector('.login-box');
+    const passwordInput = passwordUI.value.toLowerCase();
+    const correctPassword = 'zobin123'
+
+    if (usernameInput === correctUsername && passwordInput === correctPassword){
+        loginBox.style.border = '6px solid #62f77b';
+    }
+});
+
+// Login with correct username and password
+function checkInput(){
+    const usernameInput = usernameUI.value.toLowerCase();
+    const passwordInput = passwordUI.value.toLowerCase();
+    const loginBox = document.querySelector('.login-box');
+    const correctUsername = 'talmidim';
+    const correctPassword = 'zobin123'
+    if (usernameInput === correctUsername && passwordInput === correctPassword){
+        setTimeout(clientPage, 1000);
+        loginBox.style.border = '6px solid #62f77b';
+    } else {
+        console.log('incorrect username or password');
+        loginBox.style.border = '6px solid red';
+
+    }
+}
+
+function clientPage(){
+    document.location = 'user.html';
+}
+
+
+// const input = document.querySelector('input');
+// input.addEventListener('focus', function(){
+
+//     this.placeholder = '';
+// });
+// input.addEventListener('blur', function(){
+
+//     this.placeholder = "&#xf007; username";
+// });
+
+
+
