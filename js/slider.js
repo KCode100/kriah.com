dbce1a8a37828e93ab108cc95764713e9bd8883e
Kivi Corn <magiciankivi@gmail.com>
2020-12-24 23:34:19 +0200

    add jquery, slick to method.html for slider

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.single-item').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: true,
+        pauseOnHover: false,
+        arrows: false,
+        pauseOnFocus: false
+    });
+  });
