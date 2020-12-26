f20908ddfd28fcc82417005fe83e98990ef4842c
Kivi Corn <magiciankivi@gmail.com>
2020-12-26 23:26:53 +0200

    editing method.html content

--- js/slider.js
+++ js/slider.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 4000,
+        autoplaySpeed: 6500,
         dots: true,
         pauseOnHover: false,
         arrows: false,
